9f80a9100b804fef03fab68b52615e2493331fed
jjffb4a <back4app@gmx.net>
2025-05-21 20:57:39 +0200

    ???

--- app/components/ui-box.js
+++ app/components/ui-box.js
@@ -1,6 +1,6 @@
 // FILE: app/components/ui-box.js loverk 20.05.2025
-import { inject as service } from "@ember/service";import Component from '@glimmer/component';
-@service nameLoader;import { tracked } from '@glimmer/tracking';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class UiBoxComponent extends Component {
