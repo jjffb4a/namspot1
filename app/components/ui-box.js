cc14f0597f84d393c327ebdb1ac670a70cfcb7c4
jjffb4a <back4app@gmx.net>
2025-05-22 11:30:01 +0200

    Revert "with miex genNN.sh   ben9c, biggenN.sh can be removed after duplication check"
    This reverts commit dc7e8d5793d9a7471384182d9cbf688b9672fd0a.
    
    solve small conflicts on synvh ui-box and use2bis

--- app/components/ui-box.js
+++ app/components/ui-box.js
@@ -1,7 +1,10 @@
 // FILE: app/components/ui-box.js loverk 20.05.2025
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from "@ember/service";
 import { action } from '@ember/object';
+
+@service nameLoader;
 
 export default class UiBoxComponent extends Component {
   @tracked query = '';
